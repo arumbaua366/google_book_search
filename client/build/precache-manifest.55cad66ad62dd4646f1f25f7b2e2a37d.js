82e87ede89efd1d74b0f5a75abe8ccabcacdd661
ar <56378858+arumbaua366@users.noreply.github.com>
2020-06-18 23:21:17 -0700

    fix routes and pages

---
+++ client/build/precache-manifest.55cad66ad62dd4646f1f25f7b2e2a37d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d69c645584645786b11d",
+    "url": "/static/css/main.cc6aa55a.chunk.css"
+  },
+  {
+    "revision": "d69c645584645786b11d",
+    "url": "/static/js/main.33555153.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0ceb3a73e6913ded6706",
+    "url": "/static/js/2.37c40c13.chunk.js"
+  },
+  {
+    "revision": "632b94157af02af2af9936c5b2a0787f",
+    "url": "/index.html"
+  }
+];
